5ba747424ffd028f56fb887ef3ce91b35b316707
Olivier ORAND <olivier.orand@orange.com>
2015-09-15 10:39:23 +0200

    chore(): fix availability check  on JCenter

--- bin/release.sh
+++ bin/release.sh
@@ -19,18 +19,20 @@ then
 	RELEASE_CANDIDATE_VERSION=$(cat RELEASE_CANDIDATE_VERSION)
 	export REPO_NAME=$(expr ${TRAVIS_REPO_SLUG} : ".*\/\(.*\)")
 	export TAG_NAME="releases/$RELEASE_CANDIDATE_VERSION"
-	echo "TAG_NAME: $TAG_NAME\n"
+	echo "TAG_NAME: $TAG_NAME"
 	export RELEASE_NAME=$(expr "$RELEASE_CANDIDATE_VERSION" : "\(.*\)-SNAP.*")
 	export TAG_DESC="Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER - $TAG_NAME. Binaries available as maven dependencies "'[ ![Set me Up !](https://img.shields.io/badge/JCenter Bintray-Set me Up !-orange.svg) ] (https://bintray.com/package/buildSettings?pkgPath=/elpaaso/maven/'"$REPO_NAME)and also here "'[ ![Download](https://img.shields.io/badge/Download-'$RELEASE_NAME'-blue.svg) ](https://bintray.com/elpaaso/maven/'"$REPO_NAME/$RELEASE_NAME/)"
 	export GITHUB_DATA='{"tag_name":"'$TAG_NAME'","target_commitish":"master","name":"'"$RELEASE_NAME"'","body":"'"$TAG_DESC"'","draft": true,"prerelease": true}'
 	curl --silent -X POST --data "$GITHUB_DATA" https://$GH_TAGPERM@api.github.com/repos/Orange-OpenSource/$REPO_NAME/releases
-	echo "Extracted Travis repo name: $REPO_NAME\n"
+
+	echo "Extracted Travis repo name: $REPO_NAME"
 
 	JFROG_PROMOTION_URL=http://oss.jfrog.org/api/plugins/build/promote/snapshotsToBintray/$REPO_NAME/${TRAVIS_BUILD_NUMBER}
-	echo "Promotion URL to use: $JFROG_PROMOTION_URL\n"
+	echo "Promotion URL to use: $JFROG_PROMOTION_URL"
 	curl --silent -X POST -u ${BINTRAY_USER}:${BINTRAY_PASSWORD} $JFROG_PROMOTION_URL
 
-	BINTRAY_URL="https://jcenter.bintray.com/com/orange/clara/cloud/$REPO_NAME/$RELEASE_NAME/$REPO_NAME-$RELEASE_NAME.jar"
-	echo "Checking release $RELEASE_NAME is available at $BINTRAY_URL\n"
+	BINTRAY_URL="https://jcenter.bintray.com/com/orange/clara/cloud/$REPO_NAME/$RELEASE_NAME/$REPO_NAME-$RELEASE_NAME.pom"
+	echo "Checking release $RELEASE_NAME is available at $BINTRAY_URL"
+
 	wget $BINTRAY_URL
 fi
